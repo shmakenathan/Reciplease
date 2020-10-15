02651ea27194f468659dfc75f4ff63de0e704bcd
Nathan <nathan.chicha.openclassroom@gmail.com>
2020-10-15 08:22:15 +0200

    ui

---
+++ Reciplease/FridgeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Reciplease
+//
+//  Created by Nathan on 08/10/2020.
+//  Copyright © 2020 NathanChicha. All rights reserved.
+//
+
+import UIKit
+
+class FridgeViewController: UIViewController {
+
+    
+    @IBOutlet weak var ingredientUiTextField: UITextField!
+    
+    
+    @IBOutlet weak var addTapButton: UIButton!
+    
+    @IBAction func addTapButton(_ sender: Any) {
+    }
+    
+    @IBAction func searchTapButton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Reciplease/RecipeDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecipeDetailsViewController.swift
+//  Reciplease
+//
+//  Created by Nathan on 08/10/2020.
+//  Copyright © 2020 NathanChicha. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
